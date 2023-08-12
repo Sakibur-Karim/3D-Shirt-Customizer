63ae99ff28f1d4b491f32dd7d75fb384ede8e707
Sakibur-Karim <sakiburkarim@gmail.com>
2023-08-12 18:16:42 -0400

    update 2 files and delete 38 files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { motion,AnimatePresence } from 'framer-motion'
+import { useSnapshot} from 'valtio'
+
+import {
+  headContainerAnimation,
+  headTextAnimation,
+  headContentAnimation,
+  slideAnimation
+} from '../config/motion'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
