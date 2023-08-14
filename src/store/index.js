48e1b859bba3373f4706a4e53c9d51c5615c8068
Sakibur-Karim <sakiburkarim@gmail.com>
2023-08-14 09:21:59 -0400

    update Shirt.jsx and src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import { proxy } from 'valtio'
 const state = proxy({
     intro: true,
     color: '#ff0066',
-    color1: '#ddff53',
     isLogoTexture: true,
     isFullTexture: false,
     logoDecal: './threejs.png',
